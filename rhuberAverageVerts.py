c2465baffc95244d03867cbd377425174a8162f6
rhuberga <rhuberga@users.noreply.github.com>
2014-04-29 09:17:28 -0400

    Update rhuberAverageVerts.py
    update, need to pass attribute suffix and it'll be good

--- rhuberAverageVerts.py
+++ rhuberAverageVerts.py
@@ -1,47 +1,24 @@
 """
-Didn't figure out how to attach the drag context in time.
-I just copied the example from autodesk.com, not sure what's wrong
-DON'T FORGET, MAKE DRAGGER FOR BEVEL OFFSET (.1 STEP SIZE)
-AND POLY AVERAGE (1 STEP SIZE)
+rhuber
+select verts
+middle mouse drag
 """
 
 import maya.cmds as cmds
 import maya.OpenMaya as OpenMaya
 
-cmds.dragAttrContext('myDragAttrContext')
+var = "polyAverageVertex1.iterations"
 
-if cmds.filterExpand(sm=31):
+if cmds.filterExpand(sm=31): #31 = verts
     cmds.polyAverageVertex(i=0, ch=True)
-    cmds.dragAttrContext('myDragAttrContext', e=True, ct='polyAverageVertex1.iterations')
+    cmds.dragAttrContext('myDragAttrContext', e=True, ct=var)
     cmds.setToolTo('myDragAttrContext')
 
 else:
     OpenMaya.MGlobal.displayWarning("At least one vertex must be selected.")
 
-
 """
-
-import maya.cmds as cmds
-
-# Procedure called on press
-def SampleContextPress():
-    pressPosition = cmds.draggerContext( 'sampleContext', query=True, anchorPoint=True)
-    print ("Press: " + str(pressPosition))
-
-# Procedure called on drag
-def SampleContextDrag():
-    dragPosition = cmds.draggerContext( 'sampleContext', query=True, dragPoint=True)
-    button = cmds.draggerContext( 'sampleContext', query=True, button=True)
-    modifier = cmds.draggerContext( 'sampleContext', query=True, modifier=True)
-    print ("Drag: " + str(dragPosition) + " Button is " + str(button) + " Modifier is " + modifier + "\n")
-    message = str(dragPosition[0]) + ", " + str(dragPosition[1])
-    cmds.draggerContext( 'sampleContext', edit=True, drawString=message)
-
-# Define draggerContext with press and drag procedures
-cmds.draggerContext( 'sampleContext', pressCommand='SampleContextPress()', dragCommand='SampleContextDrag()', cursor='hand' );
-
-# Set the tool to the sample context created
-# Results can be observed by dragging mouse around main window
-cmds.setToolTo('sampleContext')
-
+cmds.selectMode(c=True)
+cmds.selectMode(object=True)
+cmds.delete(ch=True)
 """
