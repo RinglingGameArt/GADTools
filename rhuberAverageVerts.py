aeb8c8c280b96a503b36e16e097f93576408e932
rhuberga <rhuberga@users.noreply.github.com>
2014-04-29 12:14:09 -0400

    Update rhuberAverageVerts.py
    fixed redundant lines

--- rhuberAverageVerts.py
+++ rhuberAverageVerts.py
@@ -6,16 +6,12 @@ middle mouse drag
 
 import maya.cmds as cmds
 import maya.OpenMaya as OpenMaya
-suf = cmds.listHistory()[1].replace("polyAverageVertex", "")
-pAV = "polyAverageVertex" + str(suf) + ".iterations" #these two can be consolidated, already getting name, just add.iterations
 
 if cmds.filterExpand(sm=31): #31 = verts
     cmds.polyAverageVertex(i=0, ch=True)
-    suf = cmds.listHistory()[1].replace("polyAverageVertex", "")
-    print suf
-    cmds.dragAttrContext('myDragAttrContext', e=True, ct=pAV) #something wrong here, fix
+    pAV = cmds.listHistory()[1] + ".iterations" 
+    cmds.dragAttrContext('myDragAttrContext', e=True, ct=pAV) 
     cmds.setToolTo('myDragAttrContext')
 
 else:
     OpenMaya.MGlobal.displayWarning("At least one vertex must be selected.")
-
