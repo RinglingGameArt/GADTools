9e94b69233709f7bdec02e47294d17f14f86ec2f
rhuberga <rhuberga@users.noreply.github.com>
2014-04-29 08:32:35 -0400

    Update rhuberAverageVerts.py
    works if the object has no history

--- rhuberAverageVerts.py
+++ rhuberAverageVerts.py
@@ -11,39 +11,37 @@ import maya.OpenMaya as OpenMaya
 cmds.dragAttrContext('myDragAttrContext')
 
 if cmds.filterExpand(sm=31):
-    cmds.polyAverageVertex(i=0, ch=False)
-    cmds.dragAttrContext('myDragAttrContext', e=True, ct='polyAverageVertex.iterations')
+    cmds.polyAverageVertex(i=0, ch=True)
+    cmds.dragAttrContext('myDragAttrContext', e=True, ct='polyAverageVertex1.iterations')
     cmds.setToolTo('myDragAttrContext')
 
 else:
     OpenMaya.MGlobal.displayWarning("At least one vertex must be selected.")
 
 
-####################
-
-def SampleContextPress():
-    pressPosition = cmds.dra
-
-####################
-
 """
-contextToolsMM "MayaWindow|formLayout1|viewPanes|modelPanel4|modelPanel4|modelPanel4|modelPanel4CommandPop";
-AverageVertex;
-polyPerformAction polyAverageVertex v 0;
-polyAverageVertex -ch 1 pSphere1.vtx[8:9] pSphere1.vtx[16:18] pSphere1.vtx[24:26] pSphere1.vtx[32:34] pSphere1.vtx[40:41];
-// Result: polyAverageVertex2 //
-// Result: polyAverageVertex -ch 1 pSphere1.vtx[8:9] pSphere1.vtx[16:18] pSphere1.vtx[24:26] pSphere1.vtx[32:34] pSphere1.vtx[40:41] //
-refreshAE;
-select -addFirst polyAverageVertex2 ;
-autoUpdateAttrEd;
-updateAnimLayerEditor("AnimLayerTab");
-statusLineUpdateInputField;
-if (!`exists polyNormalSizeMenuUpdate`) {source buildDisplayMenu;} polyNormalSizeMenuUpdate;
-dR_updateCounter; dR_updateSymButton;
-dR_updateCommandPanel;
-changeToolIcon;
-dR_contextChanged;
-currentCtx;
-// Result: dragAttrContext //
-setAttr "polyAverageVertex2.iterations" 69.7;
+
+import maya.cmds as cmds
+
+# Procedure called on press
+def SampleContextPress():
+    pressPosition = cmds.draggerContext( 'sampleContext', query=True, anchorPoint=True)
+    print ("Press: " + str(pressPosition))
+
+# Procedure called on drag
+def SampleContextDrag():
+    dragPosition = cmds.draggerContext( 'sampleContext', query=True, dragPoint=True)
+    button = cmds.draggerContext( 'sampleContext', query=True, button=True)
+    modifier = cmds.draggerContext( 'sampleContext', query=True, modifier=True)
+    print ("Drag: " + str(dragPosition) + " Button is " + str(button) + " Modifier is " + modifier + "\n")
+    message = str(dragPosition[0]) + ", " + str(dragPosition[1])
+    cmds.draggerContext( 'sampleContext', edit=True, drawString=message)
+
+# Define draggerContext with press and drag procedures
+cmds.draggerContext( 'sampleContext', pressCommand='SampleContextPress()', dragCommand='SampleContextDrag()', cursor='hand' );
+
+# Set the tool to the sample context created
+# Results can be observed by dragging mouse around main window
+cmds.setToolTo('sampleContext')
+
 """
