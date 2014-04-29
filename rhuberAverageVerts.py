3fc4b872e66b2527b53b53d8aef9c26499078e51
rhuberga <rhuberga@users.noreply.github.com>
2014-04-29 12:01:24 -0400

    Update rhuberAverageVerts.py
    added another note to self

--- rhuberAverageVerts.py
+++ rhuberAverageVerts.py
@@ -7,8 +7,7 @@ middle mouse drag
 import maya.cmds as cmds
 import maya.OpenMaya as OpenMaya
 suf = cmds.listHistory()[1].replace("polyAverageVertex", "")
-print suf
-pAV = "polyAverageVertex" + str(suf) + ".iterations"
+pAV = "polyAverageVertex" + str(suf) + ".iterations" #these two can be consolidated, already getting name, just add.iterations
 
 if cmds.filterExpand(sm=31): #31 = verts
     cmds.polyAverageVertex(i=0, ch=True)
@@ -20,4 +19,3 @@ if cmds.filterExpand(sm=31): #31 = verts
 else:
     OpenMaya.MGlobal.displayWarning("At least one vertex must be selected.")
 
-print pAV
