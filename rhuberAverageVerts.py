1fdbd475f87976bb198ef203df412fa6157cc70b
rhuberga <rhuberga@users.noreply.github.com>
2014-04-29 11:02:28 -0400

    Update rhuberAverageVerts.py
    working, supposedly

--- rhuberAverageVerts.py
+++ rhuberAverageVerts.py
@@ -6,37 +6,18 @@ middle mouse drag
 
 import maya.cmds as cmds
 import maya.OpenMaya as OpenMaya
-
-pAV = "polyAverageVertex" + "1" + ".iterations"
+suf = cmds.listHistory()[1].replace("polyAverageVertex", "")
+print suf
+pAV = "polyAverageVertex" + str(suf) + ".iterations"
 
 if cmds.filterExpand(sm=31): #31 = verts
     cmds.polyAverageVertex(i=0, ch=True)
+    suf = cmds.listHistory()[1].replace("polyAverageVertex", "")
+    print suf
     cmds.dragAttrContext('myDragAttrContext', e=True, ct=pAV)
     cmds.setToolTo('myDragAttrContext')
 
 else:
     OpenMaya.MGlobal.displayWarning("At least one vertex must be selected.")
 
-"""
-cmds.selectMode(c=True)
-cmds.selectMode(object=True)
-cmds.delete(ch=True)
-"""
-
-
-
-vert = cmds.ls(sl=True)
-sel = cmds.sets()
-#fix this part
-
-
-cmds.selectMode(co=True)
-cmds.selectMode(o=True)
-sel = cmds.sets()
-suf = cmds.listHistory()[1].replace("polyAverageVertex", "")
-cmds.selectMode(co=True)
-cmds.select(sel)
-
-print suf
-cmds.listConnections(type='inputs')
-cmds.listHistory(ha=True, pdo=True, future=True)
+print pAV
