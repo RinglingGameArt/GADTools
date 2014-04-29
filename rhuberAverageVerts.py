fd27d59caaa01cc442013ed1c84b5b315939e2a4
rhuberga <rhuberga@users.noreply.github.com>
2014-04-29 10:27:47 -0400

    Update rhuberAverageVerts.py
    almost done, need to isolate "inputs" for listHistory array to get consistent results.

--- rhuberAverageVerts.py
+++ rhuberAverageVerts.py
@@ -7,11 +7,11 @@ middle mouse drag
 import maya.cmds as cmds
 import maya.OpenMaya as OpenMaya
 
-var = "polyAverageVertex1.iterations"
+pAV = "polyAverageVertex" + "1" + ".iterations"
 
 if cmds.filterExpand(sm=31): #31 = verts
     cmds.polyAverageVertex(i=0, ch=True)
-    cmds.dragAttrContext('myDragAttrContext', e=True, ct=var)
+    cmds.dragAttrContext('myDragAttrContext', e=True, ct=pAV)
     cmds.setToolTo('myDragAttrContext')
 
 else:
@@ -22,3 +22,21 @@ cmds.selectMode(c=True)
 cmds.selectMode(object=True)
 cmds.delete(ch=True)
 """
+
+
+
+vert = cmds.ls(sl=True)
+sel = cmds.sets()
+#fix this part
+
+
+cmds.selectMode(co=True)
+cmds.selectMode(o=True)
+sel = cmds.sets()
+suf = cmds.listHistory()[1].replace("polyAverageVertex", "")
+cmds.selectMode(co=True)
+cmds.select(sel)
+
+print suf
+cmds.listConnections(type='inputs')
+cmds.listHistory(ha=True, pdo=True, future=True)
